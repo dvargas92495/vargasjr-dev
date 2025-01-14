251600f9340918ab66c7ef02b296de4a053686a9
David Vargas <dvargas92495@gmail.com>
2025-01-14 08:41:03 -0500

    Auto updating agent

--- agent/src/runner.py
+++ agent/src/runner.py
@@ -1,26 +1,14 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import os
 from pathlib import Path
-import sys
 from threading import Event, Thread
 import time
 from typing import Optional
 from dotenv import load_dotenv
 
+import requests
 from src.workflows.triage_message.workflow import TriageMessageWorkflow
-
-
-def _default_logger():
-    Path("logs").mkdir(exist_ok=True)
-
-    logger = logging.getLogger(__name__)
-    log_file = f"logs/{datetime.now().strftime('%Y%m%d')}-stdout.log"
-    file_handler = logging.FileHandler(log_file)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    return logger
 
 
 class AgentRunner:
@@ -33,14 +21,19 @@ class AgentRunner:
         max_loops: Optional[int] = None,
     ):
         load_dotenv()
+        self._current_version = self._get_version()
         self._cancel_signal = cancel_signal or Event()
-        self._logger = logger or _default_logger()
+        self._logger = logger or self._default_logger()
         self._sleep_time = sleep_time
         self._max_loops = max_loops
+        self._last_updated = datetime.now()
+        self._update_interval = timedelta(minutes=1)
 
         log_level = os.getenv("LOG_LEVEL")
         if log_level:
             self._logger.setLevel(log_level)
+
+        self._logger.debug(f"Initialized agent v{self._current_version}")
 
     def run(self):
         main_thread = Thread(target=self._main_thread)
@@ -63,6 +56,64 @@ class AgentRunner:
             if self._max_loops and loops >= self._max_loops:
                 self._logger.debug("Max loops reached, stopping...")
                 self._cancel_signal.set()
+
+            if datetime.now() - self._last_updated > self._update_interval:
+                self._logger.debug("Checking for updates...")
+                self._last_updated = datetime.now()
+                release_url = "https://api.github.com/repos/dvargas92495/vargasjr-dev/releases/latest"
+                response = requests.get(release_url)
+                if response.status_code != 200:
+                    self._logger.error(f"Failed to check for updates: {response.status_code}")
+                    continue
+
+                release_data = response.json()
+                if not isinstance(release_data, dict):
+                    self._logger.error(f"Unexpected release data: {type(release_data)}")
+                    continue
+
+                latest_version = release_data.get("tag_name")
+                if not isinstance(latest_version, str):
+                    self._logger.error(f"Failed to parse release data for tag name: {release_data}")
+                    continue
+
+                if latest_version == self._current_version:
+                    self._logger.debug(f"No new version available: {latest_version}")
+                    continue
+
+                try:
+                    self._logger.info(f"New version available: {latest_version}")
+                    os.chdir("..")
+                    os.system(f"rm -Rf vargasjr_dev_agent-*")
+                    os.system(
+                        f"wget https://github.com/dvargas92495/vargasjr-dev/releases/download/v{latest_version}/vargasjr_dev_agent-{latest_version}.tar.gz"
+                    )
+                    os.system(f"tar -xzf vargasjr_dev_agent-{latest_version}.tar.gz")
+                    os.chdir(f"vargasjr_dev_agent-{latest_version}")
+                    os.system("cp ../.env .")
+                    os.system("poetry install")
+                    os.system("poetry run agent")
+                except Exception:
+                    self._logger.exception(f"Failed to update to version: {latest_version}")
+                    continue
+
+    def _get_version(self) -> str:
+        with open("pyproject.toml", "r") as f:
+            for line in f:
+                if line.startswith("version = "):
+                    return line.split("=")[1].strip().strip('"')
+        return "unknown"
+
+    def _default_logger(self) -> logging.Logger:
+        log_dir = Path.home() / ".local" / "var" / "log" / "vargas-jr"
+        log_dir.mkdir(exist_ok=True)
+
+        logger = logging.getLogger(__name__)
+        log_file = log_dir / f"v{self._current_version}" / f"stdout-{datetime.now().strftime('%Y%m%d')}.log"
+        file_handler = logging.FileHandler(str(log_file))
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        return logger
 
     def _should_run(self):
         return not self._cancel_signal.is_set()
