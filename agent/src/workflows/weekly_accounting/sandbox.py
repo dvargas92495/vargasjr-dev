682bbe1d61093656e273c33b89c0aac7393ce3cb
David Vargas <dvargas92495@gmail.com>
2025-02-02 20:59:42 -0500

    Better accounting sandbox

--- agent/src/workflows/weekly_accounting/sandbox.py
+++ agent/src/workflows/weekly_accounting/sandbox.py
@@ -1,9 +1,18 @@
+import logging
+from dotenv import load_dotenv
 from src.workflows.weekly_accounting.workflow import WeeklyAccountingWorkflow
 from vellum.workflows.sandbox import WorkflowSandboxRunner
 from vellum.workflows.inputs import BaseInputs
+from vellum.workflows.state.context import WorkflowContext
 
 if __name__ != "__main__":
     raise Exception("This is a sandbox file and should not be run as a module")
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+context = WorkflowContext()
+setattr(context, "logger", logger)
 
 runner = WorkflowSandboxRunner(
     workflow=WeeklyAccountingWorkflow(),
