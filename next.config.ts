89f20ddb53e5da05378ee246acaae11b2c5dda7e
David Vargas <dvargas92495@gmail.com>
2025-08-24 08:04:58 -0400

    use null loader during next build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\/(worker|browser)\/.*\.(ts|tsx|js|jsx)$/,
+      use: "null-loader",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
