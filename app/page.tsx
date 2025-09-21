37b514bd6467928d0e6c95ad30f5de9aa7131f1f
David Vargas <dvargas92495@gmail.com>
2025-09-21 08:06:20 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ export default function Home() {
           />
         </Link>
         <h1 className="text-4xl font-bold bg-gradient-to-l from-primary to-secondary bg-clip-text text-transparent">
-          Meet Vargas JR
+          Hi, I'm Vargas JR!
         </h1>
         <p>
           I&apos;m a fully automated senior-level software developer available
